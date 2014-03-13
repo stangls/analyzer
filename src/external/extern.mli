e586452a34117068295800631fe3d23bd4e00cba
Stefan Dangl <dangl@in.tum.de>
2014-03-13 12:34:59 +0100

    interface for extern module

---
+++ src/external/extern.mli
@@ -0,0 +1,15 @@
+
+exception InternalError
+
+(* initialize external invariants with an AST and a list of c-file names *)
+val init : Cil.file -> string list -> unit
+
+(* retrieve assertion expression and list of expressions to assert at given location *)
+val assertion_exprs : Cil.location -> Cil.exp * Cil.exp list
+
+(* create invariants in base analysis *)
+val create_base_invariants : BaseDomain.Dom.t -> unit
+
+(* write out all created invariants *)
+val write_invariants : unit -> unit
+
